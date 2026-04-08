352fc5882bddd0984a3c2432d9aeede75b75ddf3
arthur <gabrielle.alves.silva@escola.pr.gov.br>
2026-04-08 14:43:08 +0000

    v1

---
+++ imc.js
@@ -0,0 +1,14 @@
+//Calculadora de imc 
+
+let nome = "Clara";
+let peso = 65;
+let altura = 1.68;
+
+//Função que calcula e retorna o imc 
+function calcularIMC(peso, altura){
+    let imc = peso / (altura * altura)
+};
+
+let resultado = calcularIMC(peso, altura)
+
+console.log(`IMC de ${nome}: ${resultado}`);
