8173d36e5b735b721c09adbfcd81443706664d9b
arthur <gabrielle.alves.silva@escola.pr.gov.br>
2026-04-15 13:44:13 +0000

    v2

---
+++ lista.js
@@ -0,0 +1,10 @@
+const alunos = ["Rafael", "Leandro", "Alexandre"];
+
+alunos.push("Arthur");
+alunos.unshift("Jorel");
+
+console.log(alunos.indexOf("Arthur"));
+console.log(alunos.indexOf("Joel"));
+
+console.log(alunos.includes("Joel"));
+console.log(alunos.includes("Ellie"));
