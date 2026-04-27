21aed5ada63e2415ec70a29e9807bf709b50c427
arthur <gabrielle.alves.silva@escola.pr.gov.br>
2026-04-27 13:50:10 +0000

    v1

---
+++ sgp.js
@@ -0,0 +1,19 @@
+//Sistema  gerenciamentoo e personagem
+
+function calcularPontuacaoTotal(historicoDePontos) {
+    const somarRodada = function(acumulador, pontosDaFase){
+        const novaSoma = acumulador + pontosDaFase;  
+        return novaSoma;
+    };
+    
+    const pontuacaoFinal = historicoDePontos.reduce(somarRodada, 0);
+};
+function filtrarMissoesConcluidas(listaDeMissoes){
+    const verificarStatusDaMissao = function(missao){
+        const foiFinalizada = missao.status === "concluída";// === Igualdade estrita
+        return foiFinalizada;
+    };
+
+    const apenasConcluidas = listaDeMissoes.filter(verificarStatusDaMissao);
+    return apenasConcluidas;
+};
