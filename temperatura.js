65ddd42be06d1fb36d2684d51f62ad393a8d8a2b
arthur <gabrielle.alves.silva@escola.pr.gov.br>
2026-04-29 13:33:12 +0000

    vf

--- temperatura.js
+++ temperatura.js
@@ -1,6 +1,7 @@
 const cidade = "Curitiba";
 const tempCelsius = 19;
-const tempFahrenheit = tempCelsius * (9 / 5) + 32 const tempKelvin = tempCelsius + 273.15; 
+const tempFahrenheit = tempCelsius * (9 / 5) + 32; 
+const tempKelvin = tempCelsius + 273.15; 
 const variacaoDiaria = 26.3 - tempCelsius; 
 console.log(`A temperatura em ${cidade} está ${tempCelsius}Cº`); 
 console.log(`\nCelsius: ${tempCelsius} Cº`);
