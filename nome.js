e56cb5f290c851063c668e627cb75d0451242a11
arthur <gabrielle.alves.silva@escola.pr.gov.br>
2026-04-29 13:34:01 +0000

    vf

--- nome.js
+++ nome.js
@@ -1,5 +1,5 @@
 //Definindo meu nome
-let meuNome = "Kim";
-let idade = 97;
+let meuNome = "Leandro";
+let idade = 27;
 
 console.log("Olá, meu nome é: " + meuNome + ". E Tenho " + idade + " anos.")
