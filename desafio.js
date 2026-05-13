e1e63b60f8395034030f5612fa5abc423f9f0e04
arthur <gabrielle.alves.silva@escola.pr.gov.br>
2026-05-13 13:53:16 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,9 @@
+//5 Variáveis para jogador com interpolação
+let nomeJogador = "Alan"
+const nickJogo = "Joel"
+
+const nomeJogo = "The last of us"
+let objetivo = "Levar carga"
+let recompenca = "ficha de comida"
+
+console.log(`${nomeJogador} jogou ${nomeJogo} com o personagem ${nickJogo} seu objetivo era ${objetivo} e pretendia obter ${recompenca}`)
